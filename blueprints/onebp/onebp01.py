dbd3538700222973f513e51afb25fb2a01f27b45
riverhsu <river007@gmail.com>
2023-09-21 11:01:56 +0800

    Integrate BluePrint and Bootstrap Nav functions

--- blueprints/onebp/onebp01.py
+++ blueprints/onebp/onebp01.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template
 from views.bpview import bpview
 from flask_bootstrap import Bootstrap4
 
@@ -7,6 +7,10 @@ app = Flask(__name__)
 bp4 = Bootstrap4(app)
 
 app.register_blueprint(bpview)
+
+@app.route("/")
+def bphome():
+	return render_template("index.html")
 
 if __name__ == "__main__":
 	host = ["192.168.209.128",]
--- blueprints/onebp/views/bpview.py
+++ blueprints/onebp/views/bpview.py
@@ -3,10 +3,10 @@ from flask import render_template, Blueprint
 
 bpview = Blueprint("bpview", __name__)
 
-@bpview.route("/")
-def dphome():
-	return "Welcome to Blueprint Demo"
+@bpview.route("/auth")
+def authhome():
+	return render_template("auth/authhome.html")
 
-@bpview.route("/login")
-def dploing():
+@bpview.route("/auth/login")
+def authlogin():
 	return render_template("auth/login.html")
